4b738876bb8d994654ab210ae94d18efbbdbb1b9
Your Name <kamalinivaithyanathan12@gmail.com>
2026-07-25 23:33:46 +0530

    optimal to check if array is sorted -b3

---
+++ Step01-Arrays/Easy/Sort_Check.java
@@ -0,0 +1,28 @@
+package Easy;
+import java.util.*;
+
+public class Sort_Check{
+    public boolean check_array(int[] arr, int n){
+        for(int i=1;i<n;i++){
+            if(arr[i]<arr[i-1]){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+
+class main{
+    public static void main(String[] args) {
+        Sort_Check s = new Sort_Check();
+        int[] arr = {1,2,3,4,5,6};
+        int[] arr2 = {2,1,4,5,6,3};
+
+        System.out.println("arr 1 is a array : "+s.check_array(arr, arr.length));
+        System.out.println("arr 2 is sorted : "+ s.check_array(arr2, arr2.length));
+
+
+    }
+}
