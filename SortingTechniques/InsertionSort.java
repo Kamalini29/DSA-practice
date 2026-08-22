343721f4a3536783fbb69e1dc6b94a34a9ef3002
Your Name <kamalinivaithyanathan12@gmail.com>
2026-08-22 23:32:50 +0530

    Insertion Sort

---
+++ SortingTechniques/InsertionSort.java
@@ -0,0 +1,33 @@
+package SortingTechniques;
+
+public class InsertionSort{ 
+ public void insertionSort(int[] nums) {
+        int n = nums.length; // Size of the array 
+        
+        // For every element in the array 
+        for (int i = 1; i < n; i++) {
+            int key = nums[i]; // Current element as key 
+            int j = i - 1;
+            
+            // Shift elements that are greater than key by one position
+            while (j >= 0 && nums[j] > key) {
+                nums[j + 1] = nums[j];
+                j--;
+            }
+            
+            nums[j + 1] = key; // Insert key at correct position
+        }
+
+        System.out.println("After Sorting:");
+        
+        for(int i=0;i<n; i++ ){
+            System.out.println(nums[i]);
+        }
+    }
+    public static void main(String[] args){
+        int[] arr = {12,43,22,10,7,88,1,0};
+
+        InsertionSort s = new InsertionSort();
+        s.insertionSort(arr);
+    }
+}
