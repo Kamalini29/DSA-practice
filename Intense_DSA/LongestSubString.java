160e80fe2bc2c9bd505536f88e081380364b538f
Your Name <kamalinivaithyanathan12@gmail.com>
2026-09-03 22:34:13 +0530

    adding the extras and changed

---
+++ Intense_DSA/LongestSubString.java
@@ -0,0 +1,30 @@
+package Intense_DSA;
+
+import java.util.HashMap;
+
+public class LongestSubString {
+    public int findLong(String str) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        int left = 0;
+        int maxLen = 0;
+
+        for(int r=0; r<str.length();r++){
+            char c = str.charAt(r);
+            if(map.containsKey(c) && map.get(c) >= left){
+                left = map.get(c) +1;
+            }
+
+            map.put(c,r);
+            maxLen = Math.max(maxLen, r - left + 1);
+        }
+
+        return maxLen;
+    }
+    public static void main(String[] args){
+        LongestSubString s = new LongestSubString();
+        String str = "Kamaalsiniii";
+
+        int n = s.findLong(str);
+        System.out.println(n);
+    }
+}
